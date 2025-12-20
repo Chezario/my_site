c406d124cc8f8f5611ddd56bc67a9fbd059a441b
Igor Shirokov <shirokovigor@mail.ru>
2025-12-20 21:14:24 +0300

    first commit

---
+++ mysite/pages/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import date
+
+
+def index(request):
+    date1 = date.today()
+    date2 = date.fromisoformat('2025-12-31') - date1
+    content = {
+        'title': 'Main Page',
+        'content': 'Hello People',
+        'days_left': date2.days,
+    }
+    return render(request, 'index.html', content)
+
+
