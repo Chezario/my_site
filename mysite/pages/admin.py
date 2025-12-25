d0fcb1cc91f8e205fefe8efa6fea2e902f0d7e45
Igor Shirokov <shirokovigor@mail.ru>
2025-12-25 16:34:06 +0300

    Added some features

--- mysite/pages/admin.py
+++ mysite/pages/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Printers, Room
 
-# Register your models here.
+
+admin.site.register(Room)
+admin.site.register(Printers)
+
--- mysite/pages/models.py
+++ mysite/pages/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Room(models.Model):
     room_number = models.IntegerField(default=0)
     describe = models.CharField(max_length=30)
+    def __str__(self):
+        return self.describe
 
 
 class Printers(models.Model):
@@ -11,3 +13,5 @@ class Printers(models.Model):
     printer_manufacturer = models.CharField(max_length=20)
     printer_model = models.CharField(max_length=20)
     pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.printer_manufacturer
