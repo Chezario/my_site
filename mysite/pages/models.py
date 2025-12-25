0dc405b9773ab0b7c37f3aac88215bf69aa1f0ca
Igor Shirokov <shirokovigor@mail.ru>
2025-12-25 16:08:24 +0300

    Made models and superuser

--- mysite/pages/models.py
+++ mysite/pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    room_number = models.IntegerField(default=0)
+    describe = models.CharField(max_length=30)
+
+
+class Printers(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    printer_manufacturer = models.CharField(max_length=20)
+    printer_model = models.CharField(max_length=20)
+    pub_date = models.DateTimeField('date published')
