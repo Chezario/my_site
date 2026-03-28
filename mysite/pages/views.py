e02d917dd8071f231168fb2ca5fa48079455980b
Igor Shirokov <shirokovigor@mail.ru>
2026-03-28 15:42:35 +0300

    bug fixes

--- mysite/pages/views.py
+++ mysite/pages/views.py
@@ -58,17 +58,41 @@ if __name__ == '__main__':
         print(plural_form(i))
 
 
+# @login_required
+# def dashboard(request):
+#     transactions = SecurityTransaction.objects.all()
+#     current_prices = {}
+#     for transaction in transactions:
+#         current_prices[transaction.security.name] = get_real_price(transaction.security.name)
+#     print(current_prices)
+#     for transaction in transactions:
+#         transaction.current_price = current_prices.get(transaction.security.name, 0)
+#         transaction.real_price = transaction.price_per_share * (1 + transaction.broker.fee)
+#         transaction.price_to_zero = transaction.price_per_share * ((1 + transaction.broker.fee) / (1 - transaction.broker.fee))
+#     context = {
+#         'transactions': transactions,
+#         'current_prices': current_prices
+#     }
+#     # content[name2] = {
+#     #     'name': name2,
+#     #     'date': '2026-03-23',
+#     #     'start_price': '315,86',
+#     #     'current_price': get_real_price(name2)
+#     # }
+#     return render(request, 'dashboard.html', context)
+
+
 @login_required
 def dashboard(request):
     transactions = SecurityTransaction.objects.all()
     current_prices = {}
-    for transaction in transactions:
-        current_prices[transaction.security.name] = get_real_price(transaction.security.name)
-    print(current_prices)
-    for transaction in transactions:
-        transaction.current_price = current_prices.get(transaction.security.name, 0)
-        transaction.real_price = transaction.price_per_share * (1 + transaction.broker.fee)
-        transaction.price_to_zero = transaction.price_per_share * ((1 + transaction.broker.fee) / (1 - transaction.broker.fee))
+    # for transaction in transactions:
+    #     current_prices[transaction.security.name] = get_real_price(transaction.security.name)
+    # print(current_prices)
+    # for transaction in transactions:
+    #     transaction.current_price = current_prices.get(transaction.security.name, 0)
+    #     transaction.real_price = transaction.price_per_share * (1 + transaction.broker.fee)
+    #     transaction.price_to_zero = transaction.price_per_share * ((1 + transaction.broker.fee) / (1 - transaction.broker.fee))
     context = {
         'transactions': transactions,
         'current_prices': current_prices
