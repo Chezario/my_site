92c6fa2908700314cd63a84bdd5441e8bb87a5db
Igor Shirokov <shirokovigor@mail.ru>
2026-04-23 09:43:40 +0300

    added Upload File to admin pannel

--- mysite/pages/admin.py
+++ mysite/pages/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Printers, Room, Security, Broker, SecurityTransaction
+from .models import Printers, Room, Security, Broker, SecurityTransaction, UploadedFile
 
 
 admin.site.register(Room)
@@ -7,4 +7,5 @@ admin.site.register(Printers)
 admin.site.register(Security)
 admin.site.register(Broker)
 admin.site.register(SecurityTransaction)
+admin.site.register(UploadedFile)
 
