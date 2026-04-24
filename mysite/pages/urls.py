bf4d4b16c53ced42b04650bf1e8a2f240499bc91
Igor Shirokov <shirokovigor@mail.ru>
2026-04-24 08:26:40 +0300

    bf

--- mysite/pages/urls.py
+++ mysite/pages/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'pages'
@@ -11,6 +12,7 @@ urlpatterns = [
     path('<int:operation_id>/', views.operation_details, name='details'),
     path('upload/', views.upload_file, name='upload_file'),
     path('files/', views.file_list, name='file_list'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
 
 handler404 = 'pages.views.custom_404'
