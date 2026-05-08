55e79e482bf9932b223031c231202944303a1972
Igor Shirokov <shirokovigor@mail.ru>
2026-05-08 12:33:26 +0300

    bf

--- mysite/pages/admin.py
+++ mysite/pages/admin.py
@@ -1,15 +1,28 @@
 from django.contrib import admin
 from .models import Printers, Room, Security, Broker, SecurityTransaction, UploadedFile
 
-class PersonAdmin(admin.ModelAdmin):
+
+@admin.action(description="Mark selected is_on_main=False")
+def make_closed(modeladmin, request, queryset):
+    queryset.update(is_on_dashboard=False)
+
+
+class SecurityTransactionAdmin(admin.ModelAdmin):
     list_display = ['__str__', 'is_on_dashboard']
     # search_fields = ['security__name']
     list_filter = ['security', 'is_on_dashboard']
+    actions = [make_closed]
+
+    def get_changeform_initial_data(self, request):
+        return {'user': request.user}
+
+
+
 
 admin.site.register(Room)
 admin.site.register(Printers)
 admin.site.register(Security)
 admin.site.register(Broker)
-admin.site.register(SecurityTransaction, PersonAdmin)
+admin.site.register(SecurityTransaction, SecurityTransactionAdmin)
 admin.site.register(UploadedFile)
 
