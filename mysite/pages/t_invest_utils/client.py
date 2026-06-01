6e5b41a6269606fde9829daff5e88dc775d83351
Igor Shirokov <shirokovigor@mail.ru>
2026-06-01 19:01:31 +0300

    bf

--- mysite/pages/t_invest_utils/client.py
+++ mysite/pages/t_invest_utils/client.py
@@ -1,9 +1,14 @@
 from t_tech.invest import Client
 from .token import INVEST_TOKEN
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
 
 class ClientWithToken:
-    TOKEN = INVEST_TOKEN  # os.getenv("INVEST_TOKEN")
+    TOKEN = os.getenv("INVEST_TOKEN")
+    # TOKEN = INVEST_TOKEN  # os.getenv("INVEST_TOKEN")
 
     def get_client(self):
         client_obj = Client(self.TOKEN)
