8525fb6667ded5fafb02df86481214f539fdc95e
Igor Shirokov <shirokovigor@mail.ru>
2026-06-06 23:14:20 +0300

    bf

--- mysite/landing/views.py
+++ mysite/landing/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import TicketForm
+
 
 def index(request):
-    return render(request, 'landing/index.html')
+    form = TicketForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        # messages.success(request, 'Заявка сохранена. Мы свяжемся с вами по телефону.')
+        return redirect('landing:index')
+    context = {
+        form: form,
+    }
+    return render(request, 'landing/index.html', context)
